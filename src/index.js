03a5c6101b25c04f5d2e6e6fb9585b13688f94e6
Biljana Kotevska <biljanakotev@gmail.com>
2023-11-08 18:31:23 +0100

    Add logic for rendering phonetics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 let api_url = 'https://api.dictionaryapi.dev/api/v2/entries/en';
 
-let searchbarForm = document.getElementById('searchbar__form');
+let searchbarForm = document.getElementById('header__searchbar__form');
 
 let word = document.getElementById('main__header');
-let phonetics = document.getElementById('main__span');
+let phoneticsSpan = document.getElementById('main__span');
 let meaning = document.getElementById('main__meaning');
-let definitions = document.querySelectorAll('.main__listing');
 let mainListContainer = document.getElementById('main__list__container');
 let synonymsSpan = document.getElementById('main__synonyms__span');
 console.log('synonyms-container', synonymsSpan);
@@ -13,43 +12,57 @@ console.log('synonyms-container', synonymsSpan);
 searchbarForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  let searchbarInput = document.getElementById('searchbar__input').value;
+  let searchbarInput = document.getElementById('header__searchbar__input').value;
 
   if (searchbarInput === '') {
     alert('Please enter a word to search for.');
   } else {
+    // FETCH API CALL
     fetch(`${api_url}/${searchbarInput}`)
       .then((response) => response.json())
       .then((data) => {
         console.log('data', data);
-        console.log(data[0].meanings[0].definitions);
         // Handle the data from the API here
         word.innerHTML = data[0].word;
-        phonetics.innerHTML = data[0].phonetics[1].text;
+        // phonetics.innerHTML = data[0].phonetics[0].text;
+
+        let phonetics = data[0].phonetics;
+        // Using find method to find the first instance of phonetic.text
+        let firstPhoneticText = phonetics.find((phonetic) => phonetic.text);
+
+        if (firstPhoneticText) {
+          phoneticsSpan.innerHTML = firstPhoneticText.text;
+        } else {
+          phoneticsSpan.innerHTML = 'Phonetics not listed';
+        }
 
         mainListContainer.innerHTML = '';
-        const definitions = data[0].meanings[0].definitions;
+
+        // Create UL List
         const list = document.createElement('ul');
         list.className = 'main__list';
 
         const meanings = data[0].meanings;
-        console.log('meanings', meanings);
-
         meanings
           .filter((meaning) => {
             return meaning.partOfSpeech === 'noun';
           })
           .forEach((meaning) => {
+            // we are looping over the data (meanings array within the data array)
+            // Create List Item inside the forEach
+            // Updating each listitem to the meaning definition
+            // Attaching the listitem to the UL
             const listItem = document.createElement('li');
             listItem.innerHTML = meaning.definitions[0].definition;
+            console.log('definitions', meaning.definitions[0].definition);
             list.appendChild(listItem);
           });
 
-        // Append the list to the container
+        // Append the UL list to the container
         mainListContainer.appendChild(list);
 
         // Span
-        synonymsSpan.innerHTML = meanings;
+        console.log('synonyms', data[0].meanings[0].synonyms);
       })
       .catch((error) => {
         console.error('An error occurred:', error);
