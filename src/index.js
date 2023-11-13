74e3a2895b37352db2b185c1d8834e23bb97b0bc
Biljana Kotevska <biljanakotev@gmail.com>
2023-11-13 12:13:09 +0100

    Update main js logic and start css styling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-let api_url = 'https://api.dictionaryapi.dev/api/v2/entries/en';
+const api_url = 'https://api.dictionaryapi.dev/api/v2/entries/en';
 
-let searchbarForm = document.getElementById('header__searchbar__form');
-let headerSpan = document.getElementById('header__span');
-let word = document.getElementById('main__header');
-let phoneticsSpan = document.getElementById('main__span');
-let mainNounListContainer = document.getElementById('main__noun__list__container');
-let synonymsSpan = document.getElementById('main__synonyms__span');
-let playIcon = document.getElementById('main__play__icon');
+const searchbarForm = document.getElementById('header__searchbar__form');
+const headerSpan = document.getElementById('header__span');
+const word = document.getElementById('main__header');
+const phoneticsSpan = document.getElementById('main__span');
+const mainNounListContainer = document.getElementById('main__noun__list__container');
+const mainVerbListContainer = document.getElementById('main__verb__list__container');
+const synonymsSpan = document.getElementById('main__synonyms__span');
+const playIcon = document.getElementById('main__play__icon');
+const srcUrl = document.getElementById('src-url');
 
 searchbarForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -25,9 +27,9 @@ searchbarForm.addEventListener('submit', (e) => {
         word.innerHTML = data[0].word;
         // phonetics.innerHTML = data[0].phonetics[0].text;
 
-        let phonetics = data[0].phonetics;
+        const phonetics = data[0].phonetics;
         // Using find method to find the first instance of phonetic.text
-        let firstPhoneticText = phonetics.find((phonetic) => phonetic.text);
+        const firstPhoneticText = phonetics.find((phonetic) => phonetic.text);
 
         if (firstPhoneticText) {
           phoneticsSpan.innerHTML = firstPhoneticText.text;
@@ -37,7 +39,7 @@ searchbarForm.addEventListener('submit', (e) => {
 
         // Using find method to find the first instance of phonetic sound
 
-        let firstPhoneticSound = phonetics.find((phoneticSound) => {
+        const firstPhoneticSound = phonetics.find((phoneticSound) => {
           return phoneticSound.audio;
         });
 
@@ -67,40 +69,57 @@ searchbarForm.addEventListener('submit', (e) => {
             return meaning.partOfSpeech === 'noun';
           })
           .forEach((meaning) => {
-            // we are looping over the data (meanings array within the data array)
-            // Create List Item inside the forEach
-            const nounListItem = document.createElement('li');
-
-            // Loop over the defintiions array
             meaning.definitions.forEach((definitionObject) => {
               const definition = definitionObject.definition;
-              nounListItem.innerHTML += `<li>${definition}</li>`;
-            });
+              const nounListItem = document.createElement('li');
+              nounListItem.innerHTML += definition;
 
-            // Attaching the listitem to the UL
-            nounList.appendChild(nounListItem);
+              // Attaching the listitem to the UL
+              nounList.appendChild(nounListItem);
+            });
           });
 
         // Append the UL list to the container
         mainNounListContainer.appendChild(nounList);
 
         // Span meanings[0] = partofSpeech noun
-        let synonymsArr = data[0].meanings[0].synonyms.join(', ');
+        const synonymsArr = data[0].meanings[0].synonyms.join(', ');
         synonymsSpan.innerHTML = synonymsArr;
 
         // Verb meaning
+        const verbList = document.getElementById('main__verb__list');
+        const mainVerbSpan = document.getElementById('main__verb__span');
 
         meanings
-          .filter((meaning) => {
-            console.log('verb', meaning.partOfSpeech === 'verb');
-            return meaning.partOfSpeech === 'verb';
-          })
+          .filter((meaning) => meaning.partOfSpeech === 'verb')
           .forEach((meaning) => {
-            console.log(meaning.definitions);
+            // Clear the existing content of verbList
+            verbList.innerHTML = '';
+            mainVerbSpan.innerHTML = '';
 
-            const verbListItem = document.createElement('li');
-            meaning.definitions.forEach(() => {});
+            meaning.definitions.forEach((definition) => {
+              const verbDefinition = definition.definition;
+              const verbExample = definition.example;
+
+              // Create List item
+              const verbListItem = document.createElement('li');
+              verbListItem.innerHTML = verbDefinition;
+
+              // Attaching the list item to the UL
+              verbList.className = 'main__verb__list';
+              verbList.appendChild(verbListItem);
+
+              // // Verb Example
+              mainVerbSpan.innerHTML = `"${verbExample}"`;
+            });
           });
+
+        // Append the UL list to the container
+        mainVerbListContainer.appendChild(verbList);
+
+        const verbSrcUrl = data[0].sourceUrls;
+        console.log('srcURL', data[0].sourceUrls);
+        srcUrl.innerHTML = verbSrcUrl;
       })
       .catch((error) => {
         console.error('An error occurred:', error);
