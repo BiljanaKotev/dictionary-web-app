2718a3983dd4fa17d1a74eddc49213033c614dc6
Biljana Kotevska <biljanakotev@gmail.com>
2023-11-17 08:37:07 +0100

    Refactored JS and adjusted error display on header span

--- src/index.js
+++ src/index.js
@@ -65,143 +65,151 @@ searchbarForm.addEventListener('submit', (e) => {
 
   headerErrorContainer.style.display = 'none';
 
-  let searchbarInput = document.getElementById('header__searchbar__input').value;
+  let searchbarInput = headerInput.value;
 
   if (searchbarInput === '') {
-    headerSpan.innerHTML = 'Please enter a word';
+    headerSpan.innerHTML = `Whoops can't be empty...`;
+    main.style.display = 'none';
+    footer.style.display = 'none';
   } else {
     // FETCH API CALL
     fetch(`${api_url}/${searchbarInput}`)
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((data) => {
-        if (!Array.isArray(data) || data.length === 0) {
-          headerSpan.innerHTML = '';
-          main.classList.toggle('display__none');
-          footer.classList.toggle('display__none');
-          headerErrorContainer.style.display = 'block';
-          return;
-        }
-
-        headerSpan.innerHTML = '';
-        console.log('received data', data);
-        // Handle the data from the API here
-        word.innerHTML = data[0].word;
-
-        // Display the main and footer elements
-        main.style.display = 'block';
-        footer.style.display = 'block';
-
-        const phonetics = data[0].phonetics;
-        // Using find method to find the first instance of phonetic.text
-        const firstPhoneticText = phonetics.find((phonetic) => phonetic.text);
-
-        if (firstPhoneticText) {
-          phoneticsSpan.innerHTML = firstPhoneticText.text;
-        } else {
-          phoneticsSpan.innerHTML = 'Phonetics not listed';
-        }
-
-        // Using find method to find the first instance of phonetic sound
-
-        const firstPhoneticSound = phonetics.find((phoneticSound) => {
-          return phoneticSound.audio;
-        });
-
-        let firstPhoneticSoundAudio = firstPhoneticSound.audio;
-
-        // Create an HTML Audio element
-        const audioElement = new Audio(firstPhoneticSoundAudio);
-
-        // when i click the play button i want the audio to play
-
-        function playAudio() {
-          audioElement.play();
-        }
-
-        playIcon.addEventListener('click', playAudio);
-
-        mainNounListContainer.innerHTML = '';
-
-        // Create UL List
-        const nounList = document.createElement('ul');
-        nounList.className = 'main__noun__list';
-
-        const meanings = data[0].meanings;
-        meanings
-          .filter((meaning) => {
-            return meaning.partOfSpeech === 'noun';
-          })
-          .forEach((meaning) => {
-            meaning.definitions.forEach((definitionObject) => {
-              const definition = definitionObject.definition;
-
-              const nounListItem = document.createElement('li');
-              nounListItem.classList.add('pb-2');
-              nounListItem.innerHTML += definition;
-
-              // Attaching the listitem to the UL
-              nounList.appendChild(nounListItem);
-            });
-          });
-
-        // Append the UL list to the container
-        mainNounListContainer.appendChild(nounList);
-
-        // Span meanings[0] = partofSpeech noun
-        const synonymsArr = data[0].meanings[0].synonyms.join(', ');
-        synonymsSpan.innerHTML = synonymsArr;
-
-        // Verb meaning
-        const verbList = document.getElementById('main__verb__list');
-        const mainVerbSpan = document.getElementById('main__verb__span');
-
-        meanings
-          .filter((meaning) => meaning.partOfSpeech === 'verb')
-          .forEach((meaning) => {
-            // Clear the existing content of verbList
-            verbList.innerHTML = '';
-            mainVerbSpan.innerHTML = '';
-
-            meaning.definitions.forEach((definition) => {
-              const verbDefinition = definition.definition;
-              const verbExample = definition.example;
-              console.log('definition', definition);
-              console.log('verb-definition', verbDefinition);
-              console.log('verb-example', verbExample);
-
-              // Create List item
-              const verbListItem = document.createElement('li');
-              // Add bootsrap classname to list item
-              verbListItem.classList.add('pb-2');
-
-              verbListItem.innerHTML = verbDefinition;
-
-              verbList.className = 'main__verb__listing';
-
-              // Attaching the list item to the UL
-              verbList.appendChild(verbListItem);
-
-              // // Verb Example
-              if (!Array.isArray(verbExample) || verbExample.length === 0) {
-                mainVerbSpan.innerHTML = 'Example not available';
-              } else {
-                mainVerbSpan.innerHTML = `"${verbExample}"`;
-              }
-            });
-          });
-
-        // Append the UL list to the container
-        mainVerbListContainer.appendChild(verbList);
-
-        const verbSrcUrl = data[0].sourceUrls;
-        srcUrl.innerHTML = verbSrcUrl;
-
-        srcLink.href = verbSrcUrl;
+        processData(data);
       })
       .catch((error) => {
-        console.error('An error occurred:', error);
+        throw new error('Failed to fetch data');
       });
   }
 });
+
+function processData(data) {
+  console.log('data', data);
+  if (!Array.isArray(data) || data.length === 0) {
+    handleInvalidData();
+    return;
+  } else {
+    handleValidData();
+  }
+
+  function handleValidData() {
+    headerSpan.innerHTML = '';
+
+    // Handle the data from the API here
+    word.innerHTML = data[0].word;
+    main.style.display = 'block';
+    footer.style.display = 'block';
+  }
+
+  function handleInvalidData() {
+    headerSpan.innerHTML = '';
+    main.style.display = 'none';
+    footer.style.display = 'none';
+    headerErrorContainer.style.display = 'block';
+  }
+
+  const phonetics = data[0].phonetics;
+  // Using find method to find the first instance of phonetic.text
+  const firstPhoneticText = phonetics.find((phonetic) => phonetic.text);
+
+  if (firstPhoneticText) {
+    phoneticsSpan.innerHTML = firstPhoneticText.text;
+  } else {
+    phoneticsSpan.innerHTML = 'Phonetics not listed';
+  }
+
+  // Using find method to find the first instance of phonetic sound
+
+  const firstPhoneticSound = phonetics.find((phoneticSound) => {
+    return phoneticSound.audio;
+  });
+
+  let firstPhoneticSoundAudio = firstPhoneticSound.audio;
+
+  // Create an HTML Audio element
+  const audioElement = new Audio(firstPhoneticSoundAudio);
+
+  // when I click the play button I want the audio to play
+
+  function playAudio() {
+    audioElement.play();
+  }
+
+  playIcon.addEventListener('click', playAudio);
+
+  mainNounListContainer.innerHTML = '';
+
+  // Create UL List
+  const nounList = document.createElement('ul');
+  nounList.className = 'main__noun__list';
+
+  const meanings = data[0].meanings;
+  meanings
+    .filter((meaning) => {
+      return meaning.partOfSpeech === 'noun';
+    })
+    .forEach((meaning) => {
+      meaning.definitions.forEach((definitionObject) => {
+        const definition = definitionObject.definition;
+
+        const nounListItem = document.createElement('li');
+        nounListItem.classList.add('pb-2');
+        nounListItem.innerHTML += definition;
+
+        // Attaching the listitem to the UL
+        nounList.appendChild(nounListItem);
+      });
+    });
+
+  // Append the UL list to the container
+  mainNounListContainer.appendChild(nounList);
+
+  // Span meanings[0] = partofSpeech noun
+  const synonymsArr = data[0].meanings[0].synonyms.join(', ');
+  synonymsSpan.innerHTML = synonymsArr;
+
+  // Verb meaning
+  const verbList = document.getElementById('main__verb__list');
+  const mainVerbSpan = document.getElementById('main__verb__span');
+
+  meanings
+    .filter((meaning) => meaning.partOfSpeech === 'verb')
+    .forEach((meaning) => {
+      // Clear the existing content of verbList
+      verbList.innerHTML = '';
+      mainVerbSpan.innerHTML = '';
+
+      meaning.definitions.forEach((definition) => {
+        const verbDefinition = definition.definition;
+        const verbExample = definition.example;
+
+        // Create List item
+        const verbListItem = document.createElement('li');
+        // Add bootsrap classname to list item
+        verbListItem.classList.add('pb-2');
+
+        verbListItem.innerHTML = verbDefinition;
+
+        verbList.className = 'main__verb__listing';
+
+        // Attaching the list item to the UL
+        verbList.appendChild(verbListItem);
+
+        // // Verb Example
+        if (!Array.isArray(verbExample) || verbExample.length === 0) {
+          mainVerbSpan.innerHTML = 'Example not available';
+        } else {
+          mainVerbSpan.innerHTML = `"${verbExample}"`;
+        }
+      });
+    });
+
+  // Append the UL list to the container
+  mainVerbListContainer.appendChild(verbList);
+
+  const verbSrcUrl = data[0].sourceUrls;
+  srcUrl.innerHTML = verbSrcUrl;
+
+  srcLink.href = verbSrcUrl;
+}
